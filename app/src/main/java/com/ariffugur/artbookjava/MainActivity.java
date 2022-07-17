80321af3ecca814d7346af7d5b18ac67813f1ad8
arif <96446876+ariffugur@users.noreply.github.com>
2022-07-17 04:30:06 +0300

    update 0.2

--- app/src/main/java/com/ariffugur/artbookjava/MainActivity.java
+++ app/src/main/java/com/ariffugur/artbookjava/MainActivity.java
@@ -1,21 +1,42 @@
 package com.ariffugur.artbookjava;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.ariffugur.artbookjava.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        View view=binding.getRoot();
+        setContentView(view);
     }
-    public void save(View view){
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.art_menu,menu);
+        return super.onCreateOptionsMenu(menu);
     }
-    public void selectImage(View view){
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.add_art){
+            Intent intent=new Intent(this,ArtActivity.class);
+            startActivity(intent);
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
